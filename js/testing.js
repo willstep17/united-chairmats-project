55d2f4571e59769d271c1f0748b15e258433f828
William Stephens <william.a.stephens17@gmail.com>
2019-12-30 16:37:38 -0600

    working on validate-extension-right.js

---
+++ js/testing.js
@@ -0,0 +1,32 @@
+(function(){
+"use strict";
+
+const axesDimensions = ["1", "1"];
+
+const chairmat = {
+    isSquare: false,
+    largeAxisMax: undefined,
+    smallAxisMax: undefined,
+    sides: [
+        "","","","","","",
+    ],
+    determineAxes: function(dimensions) {
+        if(dimensions[0] === dimensions[1]) {
+            this.isSquare = true;
+            this.largeAxisMax = dimensions[0];
+            this.smallAxisMax = dimensions[0];
+        } else if (dimensions[0] > dimensions[1]) {
+            this.largeAxisMax = dimensions[0];
+            this.smallAxisMax = dimensions[1];
+        } else {
+            this.largeAxisMax = dimensions[1];
+            this.smallAxisMax = dimensions[0];
+        }
+    },
+};
+
+chairmat.determineAxes(axesDimensions);
+
+console.log(chairmat);
+
+}());
