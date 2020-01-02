6b8e4e05e1b0b0d1d286f0a926984e8f61ff0a86
William Stephens <william.a.stephens17@gmail.com>
2020-01-02 17:25:45 -0600

    .

--- js/notepad.js
+++ js/notepad.js
@@ -15,11 +15,13 @@ window.onload = function () {
 }
 </script>
 
+
 // make the current product object available to the front-end app
-$('body').data("current_product", {{ product | json }});
+let dataBody = $('body').data("current_product", {{ product | json }});
+console.log(dataBody);
 
 // make the currently selected variant object available to the front-end app
-$('body').data("selected_variant", variant);
+let product$('body').data("selected_variant", variant);
 
 // hide or show form depending on which variant is selected
 if ((variant.inventory_policy == 'deny') && (! variant.available)) {
@@ -27,4 +29,67 @@ if ((variant.inventory_policy == 'deny') && (! variant.available)) {
     $('#myapp_form').show();
 } else {
     $('#myapp_container').hide();
+}
+
+
+VIG.registerSelectChange = function()
+{
+    //get all select elements and check options for values
+    var variantIDs = Object.keys(VIG.variants);
+    var selects = $('select');
+
+    //find the select element which contains the variant ids as values
+    //this should be available in every addtocart form, since the variant id
+    //is needed to put the item into the cart
+    var variantSelect = findVariantSelect(variantIDs,selects);
+
+    //if found monitor the select element for value changes
+    //unfortunately the jQuery val() function which is used by most themes to change the
+    //value of the hidden select field doesn't trigger the change event
+    //so we have to check very 500ms and compare the values, this is the not so
+    //elegant part.
+
+    if(variantSelect != undefined) {
+        console.log("variantSelect found. Registering change event.");
+        var lastVal = variantSelect.val();
+
+        VIG.triggerInterval = setInterval(function() {
+            var newVal = variantSelect.val();
+            if(newVal !== lastVal) {
+                // it changed, fire an event or process it
+                variant = {id:newVal};
+                console.log('Variant changed!!!');
+
+
+                //if the value has changed, trigger our event
+                $(document).trigger({
+                    type: 'variantImageChange',
+                    variant: variant,
+                    internalVIPTrigger: true
+                });
+            }
+            lastVal = newVal;
+        }, 500);
+
+    }
+}
+
+
+var findVariantSelect = function(variantIDs,selects) {
+    var variantSelect = undefined;
+
+    selects.each(function(index,element){
+        var options = $(element).children('option'),
+            isVariantSelect = false;
+
+        options.each(function(index,element){
+            if(variantIDs.includes($(element).val())) {
+                isVariantSelect = true;
+            }
+        });
+        if(isVariantSelect) {
+            variantSelect = $(element);
+        }
+    });
+    return variantSelect;
 }
